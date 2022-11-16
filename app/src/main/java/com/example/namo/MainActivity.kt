717a1686554a6eb392bd94fbe9fa800df4b5f0db
joeun-01 <jjy020121@gmail.com>
2022-11-16 15:22:34 +0900

    [FEAT] LoginFragment 로그인 레이아웃 추가

--- app/src/main/java/com/example/namo/MainActivity.kt
+++ app/src/main/java/com/example/namo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.namo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.namo.databinding.ActivityMainBinding
+import com.example.namo.login.TermsFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/namo/login/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.namo.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.namo.R
+import com.example.namo.databinding.FragmentLoginBinding
+
+class LoginFragment: Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+}
