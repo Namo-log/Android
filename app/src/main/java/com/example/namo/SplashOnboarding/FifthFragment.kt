f1577b890294a9f55c7407ebcf06def4a06abec7
Ginieee <kaj1226@naver.com>
2022-11-16 16:02:55 +0900

    [FEAT] #1 - Splash, Onboarding, Onboarding 하위 프래그먼트 생성

---
+++ app/src/main/java/com/example/namo/SplashOnboarding/FifthFragment.kt
@@ -0,0 +1,52 @@
+package com.example.namo.SplashOnboarding
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.namo.R
+import com.example.namo.databinding.FragmentOnboarding5Binding
+
+class FifthFragment : Fragment() {
+
+    private var _binding : FragmentOnboarding5Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOnboarding5Binding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.onboarding5StartBtn.setOnClickListener {
+            //로그인 화면으로 이동
+            onBoardingFinished()
+        }
+
+    }
+
+    private fun onBoardingFinished(){
+        val prefs = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        prefs.edit().putBoolean("finished",true).apply()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = FifthFragment()
+    }
+}
---
+++ app/src/main/java/com/example/namo/SplashOnboarding/SplashFragment.kt
@@ -0,0 +1,52 @@
+package com.example.namo.SplashOnboarding
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.namo.R
+import com.example.namo.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding : FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            //앱 처음인지, 로그인 되어있는지 유무 판단해야 됨
+            if (isOnBoardingFinished()){
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+            }
+        }, 1500)
+    }
+
+    private fun isOnBoardingFinished() : Boolean {
+        val prefs = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return prefs.getBoolean("finished", false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
