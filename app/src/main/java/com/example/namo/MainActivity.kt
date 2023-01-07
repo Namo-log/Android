fdfcb1ec8b076b20b70b817a42fa8679496e9a03
Ginieee <kaj1226@naver.com>
2023-01-07 22:03:18 +0900

    [FIX] #2 스플래시 Android12 api로 수정

--- app/src/main/java/com/example/namo/MainActivity.kt
+++ app/src/main/java/com/example/namo/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.namo
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.namo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding : ActivityMainBinding
+    private lateinit var splashScreen: SplashScreen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen = installSplashScreen()
+        startSplash()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initNavigation()
+    }
+
+    private fun startSplash() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 4f, 1f)
+            val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 4f, 1f)
+
+            ObjectAnimator.ofPropertyValuesHolder( splashScreenView.iconView, scaleX, scaleY).run {
+                interpolator = AnticipateInterpolator()
+                duration = 1000L
+                doOnEnd {
+                    splashScreenView.remove()
+                }
+                start()
+            }
+        }
     }
 
     private fun initNavigation() {
