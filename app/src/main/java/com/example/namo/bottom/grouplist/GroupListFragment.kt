d71ee0f3144452cf71d747396288f70ab2b54a5a
nahy-512 <hedghog1004@naver.com>
2023-01-07 21:30:19 +0900

    [FEAT] 그룹 옵션 메뉴 & 그룹 생성 다이얼로그 추가

--- app/src/main/java/com/example/namo/bottom/grouplist/GroupListFragment.kt
+++ app/src/main/java/com/example/namo/bottom/grouplist/GroupListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.namo.bottom.grouplist
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.namo.R
@@ -16,6 +18,12 @@ class GroupListFragment : Fragment() {
     lateinit var binding: FragmentGroupListBinding
 
     private var groupDatas = ArrayList<Group>()
+    lateinit var mContext : Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +39,11 @@ class GroupListFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        initRecyclerView()
+        onClickMenu()
+    }
+
+    private fun initRecyclerView() {
         // 어댑터와 데이터리스트 연결
         val groupRVAdapter = GroupListRVAdapter(groupDatas)
         binding.groupListRv.adapter = groupRVAdapter
@@ -52,6 +65,31 @@ class GroupListFragment : Fragment() {
             add(Group("나모 안드 캘린더", R.color.notyetGray, 3, "보리, 앨리, 지니, 코코아"))
             add(Group("가족 캘린더", R.color.notyetGray, 4, "엄마, 아빠, 오빠, 나"))
         }
+    }
 
+    //메뉴 클릭시
+    private fun onClickMenu(){
+        binding.groupMoreIv.setOnClickListener {
+            var popupMenu = PopupMenu(mContext, it)
+            popupMenu.menuInflater.inflate(R.menu.group_option_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                if (menuItem.itemId === R.id.menu_item_create_group) {
+                    Toast.makeText(activity, "그룹 생성", Toast.LENGTH_SHORT).show()
+                    showCreateGroupDialog()
+                } else if (menuItem.itemId === R.id.menu_item_input_group_code) {
+                    Toast.makeText(activity, "그룹 코드 입력", Toast.LENGTH_SHORT).show()
+                    //startActivity(Intent(activity, InputGroupCodeActivity::class.java))
+                }
+                false
+            }
+            popupMenu.show()
+        }
+    }
+
+    // 그룹 생성
+    private fun showCreateGroupDialog() {
+        CreateGroupDialog(requireContext()) {
+            //viewModel.setName(it)
+        }.show()
     }
 }
