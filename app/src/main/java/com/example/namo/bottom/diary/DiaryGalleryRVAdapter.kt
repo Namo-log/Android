7360c6f0a4198f5cfe8e4aa3365736d86b33a4f9
SUBIN PARK <psb8909@naver.com>
2023-01-26 17:18:43 +0900

    [FEAT] #4 이미지 glide 추가

--- app/src/main/java/com/example/namo/bottom/diary/DiaryGalleryRVAdapter.kt
+++ app/src/main/java/com/example/namo/bottom/diary/DiaryGalleryRVAdapter.kt
@@ -1,13 +1,19 @@
+package com.example.namo.bottom.diary
+
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.example.namo.bottom.diary.Gallery
+import com.bumptech.glide.Glide
 import com.example.namo.databinding.ItemDiaryGalleryBinding
 
-class DiaryGalleryRVAdapter(
-    private val imgList:MutableList<Gallery>):
-    RecyclerView.Adapter<DiaryGalleryRVAdapter.ViewHolder>(){
 
+class DiaryGalleryRVAdapter(
+    private val imgList:MutableList<Gallery>,
+    private val context: Context
+):
+    RecyclerView.Adapter<DiaryGalleryRVAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding: ItemDiaryGalleryBinding = ItemDiaryGalleryBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup,false)
@@ -15,16 +21,37 @@ class DiaryGalleryRVAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(imgList[position])
+
+        Glide.with(context)
+            .load(imgList[position].img)
+            .into(holder.image)
+
+        //Preload
+        if (position <= imgList.size) {
+            val endPosition = if (position + 1 > imgList.size) {
+                imgList.size
+            } else {
+                position + 1
+            }
+            imgList.subList(position, endPosition )
+                .map {
+                    it }
+                .forEach {
+                    preload(context, it.img)
+            }
+        }
     }
 
     override fun getItemCount(): Int = imgList.size
 
     inner class ViewHolder(val binding: ItemDiaryGalleryBinding): RecyclerView.ViewHolder(binding.root){
 
-        fun bind(item: Gallery) {
-          binding.galleryImgIv.setImageResource(item.img)
-        }
+        val image=binding.galleryImgIv
+    }
 
+    private fun preload(context: Context, img:Int) {
+        Glide.with(context).load(img)
+            .preload(150, 150)
     }
 }
+
