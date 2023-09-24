57e6402f44b114804a25612f39e8eac4d852be3b
nahy-512 <hedghog1004@naver.com>
2023-09-25 04:18:46 +0900

    [FEAT] 커스텀 My 화면에 기본 팔레트 추가

--- app/src/main/java/com/example/namo/ui/bottom/custom/CustomMyFragment.kt
+++ app/src/main/java/com/example/namo/ui/bottom/custom/CustomMyFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.namo.R
+import com.example.namo.data.entity.custom.Palette
+import com.example.namo.databinding.FragmentCustomMyBinding
+import com.example.namo.ui.bottom.custom.adapter.PaletteRVAdapter
 
 class CustomMyFragment : Fragment() {
+
+    lateinit var binding : FragmentCustomMyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_my, container, false)
+        binding = FragmentCustomMyBinding.inflate(inflater, container, false)
+
+        // 팔레트 페이지랑 동일한 데이터
+        val paletteDatas = arrayListOf(
+            Palette("기본 팔레트",
+                arrayListOf(
+                    R.color.palette1, R.color.palette2, R.color.palette3, R.color.palette4, R.color.palette5,
+                    R.color.palette6, R.color.palette7, R.color.palette8, R.color.palette9, R.color.palette10)),
+        )
+
+        //어댑터 연결
+        binding.customMyPaletteRv.apply {
+            adapter = PaletteRVAdapter(requireContext()).build(paletteDatas)
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+
+        return binding.root
     }
 
 }
