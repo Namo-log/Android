9bd254d66e7d736c88ba3deedbe299f1360339f5
Nahyun Kim <hedghog1004@naver.com>
2024-03-30 02:16:44 +0900

    ✨ Feat: CategoryColor enum class 구현
    - 팔레트 소속, paletteId, hex 컬러 저장
    
    Related to: #151

---
+++ app/src/main/java/com/mongmong/namo/presentation/config/CategoryColor.kt
@@ -0,0 +1,22 @@
+package com.mongmong.namo.presentation.config
+
+enum class BelongType { BELONG1, BELONG2 }
+
+enum class CategoryColor(val belongType: BelongType, val paletteId: Int, val hexCode: String) {
+    /** 기본 색상 */
+    SCHEDULE(BelongType.BELONG1, 1, "#DE8989"),
+    YELLOW(BelongType.BELONG1, 2, "#E1B000"),
+    BLUE(BelongType.BELONG1, 3, "#5C8596"),
+    MOIM(BelongType.BELONG1, 4, "#DA6022"),
+    /** 기본 팔레트 */
+    DEFAULT_PALETTE_COLOR1(BelongType.BELONG2, 5, "#EB5353"),
+    DEFAULT_PALETTE_COLOR2(BelongType.BELONG2, 6, "#EC9B3B"),
+    DEFAULT_PALETTE_COLOR3(BelongType.BELONG2, 7, "#FBCB0A"),
+    DEFAULT_PALETTE_COLOR4(BelongType.BELONG2, 8, "#96BB7C"),
+    DEFAULT_PALETTE_COLOR5(BelongType.BELONG2, 9, "#5A8F7B"),
+    DEFAULT_PALETTE_COLOR6(BelongType.BELONG2, 10, "#82C4C3"),
+    DEFAULT_PALETTE_COLOR7(BelongType.BELONG2, 11, "#187498"),
+    DEFAULT_PALETTE_COLOR8(BelongType.BELONG2, 12, "#8571BF"),
+    DEFAULT_PALETTE_COLOR9(BelongType.BELONG2, 13, "#E36488"),
+    DEFAULT_PALETTE_COLOR10(BelongType.BELONG2, 14, "#858585"),
+}
