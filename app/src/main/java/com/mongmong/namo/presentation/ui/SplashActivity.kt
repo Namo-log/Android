d4e3aa61b27500700bf152dc577043951b0bc15b
edv-Shin <edvedv613@gmail.com>
2024-05-21 21:24:03 +0900

    ✨Feat: 인앱업데이트 추가
    Related to: #215

--- app/src/main/java/com/mongmong/namo/presentation/ui/SplashActivity.kt
+++ app/src/main/java/com/mongmong/namo/presentation/ui/SplashActivity.kt
@@ -2,10 +2,20 @@ package com.mongmong.namo.presentation.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.mongmong.namo.databinding.ActivitySplashBinding
 import com.mongmong.namo.presentation.config.ApplicationClass
 import com.mongmong.namo.presentation.ui.login.AuthViewModel
@@ -21,6 +31,26 @@ class SplashActivity : AppCompatActivity() {
     private val isDataLoaded = MutableStateFlow(false)
     private val viewModel : AuthViewModel by viewModels()
 
+    private lateinit var appUpdateManager: AppUpdateManager
+
+    private val updateActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // 업데이트 성공
+            autoLogin()
+        } else {
+            // 업데이트 실패
+            Toast.makeText(this, "업데이트 실패", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val installStateUpdatedListener = InstallStateUpdatedListener { state ->
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            popupCompleteUpdate()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashScreen = installSplashScreen()
@@ -28,11 +58,74 @@ class SplashActivity : AppCompatActivity() {
             !isDataLoaded.value
         }
 
-        autoLogin()
         initObserve()
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        checkForUpdate()
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun checkForUpdate() {
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        Log.d("checkForUpdate", "checkForUpdate")
+
+        appUpdateInfoTask.addOnSuccessListener { info ->
+            if (info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                when (info.updatePriority()) {
+                    HIGH -> {
+                        if (info.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                            appUpdateManager.startUpdateFlowForResult(
+                                info,
+                                updateActivityResultLauncher,
+                                AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build())
+                        }
+                    }
+                    MEDIUM -> {
+                        autoLogin()
+                        if (info.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                            appUpdateManager.startUpdateFlowForResult(
+                                info,
+                                updateActivityResultLauncher,
+                                AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build())
+                        }
+                        appUpdateManager.registerListener(installStateUpdatedListener)
+                    }
+                    LOW -> {
+                        autoLogin()
+                    }
+                }
+            } else {
+                Log.d("checkForUpdate", "No updateAvailability")
+                autoLogin()
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("checkForUpdate", "Update check failed", exception)
+        }
+    }
+
+    private fun popupCompleteUpdate() {
+        Toast.makeText(
+            this,
+            "다운로드 완료. 앱을 재시작 해주세요.",
+            Toast.LENGTH_LONG
+        ).show()
+
+        appUpdateManager.completeUpdate()
+    }
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                popupCompleteUpdate()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        appUpdateManager.unregisterListener(installStateUpdatedListener)
     }
 
     private fun autoLogin() {
@@ -60,5 +153,8 @@ class SplashActivity : AppCompatActivity() {
 
     companion object {
         const val OK_CODE = 200
+        const val HIGH = 3
+        const val MEDIUM = 2
+        const val LOW = 1
     }
 }
