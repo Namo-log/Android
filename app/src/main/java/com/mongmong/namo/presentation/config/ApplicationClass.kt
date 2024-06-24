2e08ae0e28addc6eb3be3fa91cb918e13a29c802
Nahyun Kim <hedghog1004@naver.com>
2024-06-24 22:09:28 +0900

    🔖Release: 1.0.4

--- app/src/main/java/com/mongmong/namo/presentation/config/ApplicationClass.kt
+++ app/src/main/java/com/mongmong/namo/presentation/config/ApplicationClass.kt
@@ -44,7 +44,7 @@ class ApplicationClass: Application() {
             get() = instance.getSharedPreferences("NAMO", MODE_PRIVATE)
 
         // 버전
-        const val VERSION = "1.0.2"
+        const val VERSION = "1.0.4"
 
         // JWT Token Header 키 값
         const val X_ACCESS_TOKEN = "X_ACCESS_TOKEN"
