4f3dc390b3140f71b5d0e44a8b4b0b8c32fc6336
edv-Shin <edvedv613@gmail.com>
2024-07-19 23:13:17 +0900

    ♻️ Refactor: 그룹 상세 데이터바인딩 적용
    Related to: #264

--- app/src/main/java/com/mongmong/namo/presentation/ui/group/GroupInfoActivity.kt
+++ app/src/main/java/com/mongmong/namo/presentation/ui/group/GroupInfoActivity.kt
@@ -22,36 +22,34 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class GroupInfoActivity : AppCompatActivity(), ConfirmDialogInterface {
 
-    private lateinit var binding : ActivityGroupInfoBinding
-
-    private lateinit var groupInfoMemberRVAdapter : GroupInfoMemberRVAdapter
-
-    private val viewModel : GroupViewModel by viewModels()
+    private lateinit var binding: ActivityGroupInfoBinding
+    private val viewModel: GroupViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGroupInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
         viewModel.setGroup(intent.getSerializableExtra("group") as Group)
+
         setAdapter()
         setClickListener()
-        setGroupInfo()
         setEditTextListener()
-
         initObserve()
     }
 
     private fun setAdapter() {
         binding.groupInfoMemberRv.layoutManager = GridLayoutManager(this, 2)
-        groupInfoMemberRVAdapter = GroupInfoMemberRVAdapter(
-            viewModel.groupInfo.value?.groupMembers ?: emptyList())
+        val groupMembers = viewModel.groupInfo.value?.groupMembers ?: emptyList()
+        val groupInfoMemberRVAdapter = GroupInfoMemberRVAdapter(groupMembers)
         binding.groupInfoMemberRv.adapter = groupInfoMemberRVAdapter
     }
 
     private fun setClickListener() {
         binding.groupInfoCloseBtn.setOnClickListener { finish() }
-
         binding.groupInfoSaveBtn.setOnClickListener {
             viewModel.updateGroupName(binding.groupInfoGroupNameContentEt.text.toString())
         }
@@ -63,75 +61,55 @@ class GroupInfoActivity : AppCompatActivity(), ConfirmDialogInterface {
     }
 
     private fun showLeaveDialog() {
-        // 탈퇴 확인 다이얼로그
         val title = "정말 모임에서 탈퇴하시겠어요?"
         val content = "탈퇴하더라도\n이전 모임 일정은 사라지지 않습니다."
 
-        val dialog = ConfirmDialog(this@GroupInfoActivity, title, content, "삭제", 0)
+        val dialog = ConfirmDialog(this, title, content, "삭제", 0)
         dialog.isCancelable = false
         dialog.show(this.supportFragmentManager, "ConfirmDialog")
     }
 
-    private fun setGroupInfo() {
-        val group = viewModel.groupInfo.value
-        with(binding) {
-            groupInfoGroupNameContentEt.setText(group?.groupName)
-            groupInfoMemberHeaderContentTv.text = group?.groupMembers?.size.toString()
-            groupInfoCodeTv.isSelected = true
-            groupInfoCodeTv.text = group?.groupCode
-        }
-
-    }
-
     private fun setEditTextListener() {
-        binding.groupInfoGroupNameContentEt.setOnKeyListener { view, keyCode, event ->
-            // Enter Key Action
-            if (event.action == KeyEvent.ACTION_DOWN
-                && keyCode == KeyEvent.KEYCODE_ENTER
-            ) {
-                // 키보드 내리기
+        binding.groupInfoGroupNameContentEt.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                 val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(binding.groupInfoGroupNameContentEt.windowToken, 0)
                 true
             }
-
             false
         }
     }
 
     private fun initObserve() {
         viewModel.updateGroupNameResult.observe(this) {
-            if(it.result != 0L) {
+            if (it.result != 0L) {
                 Toast.makeText(this, "모임 이름이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-                val resultIntent = Intent(this, GroupCalendarActivity::class.java).apply {
-                    putExtra("groupName", binding.groupInfoGroupNameContentEt.text.toString()) // 사용자가 이름을 바꿨음을 알려줌
+                val resultIntent = Intent().apply {
+                    putExtra("groupName", binding.groupInfoGroupNameContentEt.text.toString())
                 }
                 setResult(Activity.RESULT_OK, resultIntent)
+                finish()
             }
-            finish()
         }
         viewModel.deleteMemberResult.observe(this) {
-            if(it == 200) {
+            if (it == 200) {
                 Toast.makeText(this, "${viewModel.groupInfo.value?.groupMembers} 모임에서 탈퇴하였습니다.", Toast.LENGTH_SHORT).show()
-
-                val resultIntent = Intent(this, GroupCalendarActivity::class.java).apply {
-                    putExtra("leave", true) // 사용자가 탈퇴했음을 알려줌
+                val resultIntent = Intent().apply {
+                    putExtra("leave", true)
                 }
                 setResult(Activity.RESULT_OK, resultIntent)
+                finish()
             }
-            finish()
         }
     }
-    private fun copyTextToClipboard(text : String) {
+
+    private fun copyTextToClipboard(text: String) {
         val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clipData = ClipData.newPlainText("text", text)
-
         clipboardManager.setPrimaryClip(clipData)
     }
 
-
     override fun onClickYesButton(id: Int) {
-        // 탈퇴 진행
         viewModel.deleteGroupMember()
     }
 }
