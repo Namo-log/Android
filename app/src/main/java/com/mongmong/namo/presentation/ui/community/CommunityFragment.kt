264c6ab3b39b3db30069ebe11fad0b75b5951c73
Nahyun Kim <hedghog1004@naver.com>
2024-08-20 04:00:19 +0900

    ♻️ Refactor: BottomNavigation 아이콘 변경
    - 아이콘 사이즈 30 -> 40
    - ui 패키지에 community, shop 폴더 추가
    - nav_bottomnav 그래프 정리
    
    Related to: #301

---
+++ app/src/main/java/com/mongmong/namo/presentation/ui/community/CommunityFragment.kt
@@ -0,0 +1,12 @@
+package com.mongmong.namo.presentation.ui.community
+
+import com.mongmong.namo.R
+import com.mongmong.namo.databinding.FragmentCommunityBinding
+import com.mongmong.namo.presentation.config.BaseFragment
+
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>(R.layout.fragment_community) {
+
+    override fun setup() {
+
+    }
+}
